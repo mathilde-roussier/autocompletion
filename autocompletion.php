a56b054d843e8e43abb61c5841e0689912eca7f5
Mathilde <mathilde.roussier@laplateforme.io>
2020-04-01 11:09:27 +0200

    function bdd (recuperation * bdd) + function recherche (recuperation recherche bdd)

--- autocompletion.php
+++ autocompletion.php
@@ -1,8 +1,54 @@
-<?php 
+<?php
 
-// Requête SQL recup base de donnée recherche 
-// concat(nom,style,dernier_album)
+$_GET['recherche'] = 'belge';
+$_GET['recup'] = '1';
 
-// Requête SQL recup ID qui correspond  
+// Requête SQL recup base de donnée
+function bdd()
+{
+    try {
+        $con = new PDO('mysql:host=localhost;dbname=autocompletion;charset=utf8', 'root', '');
+    } catch (Exception $e) {
+        die('Erreur : ' . $e->getMessage());
+    }
 
-?>
+    $recup = $con->query("SELECT * from artistes");
+
+    $bdd = [];
+
+    while ($donnees = $recup->fetch(PDO::FETCH_ASSOC)) {
+
+        array_push($bdd, json_encode($donnees));
+    }
+
+    echo json_encode($bdd);
+}
+
+// Requête SQL recup résultat recherche
+function recherche($recherche)
+{
+    try {
+        $con = new PDO('mysql:host=localhost;dbname=autocompletion;charset=utf8', 'root', '');
+    } catch (Exception $e) {
+        die('Erreur : ' . $e->getMessage());
+    }
+
+    $recup = $con->query("SELECT nom FROM artistes WHERE CONCAT(nom,nationalite,style_musicale) LIKE '%" . $recherche . "%' ");
+
+    $resultat = [];
+
+    while ($donnees = $recup->fetch(PDO::FETCH_ASSOC)) {
+        array_push($resultat, json_encode($donnees));
+    }
+    echo json_encode($resultat);
+}
+
+// Activation des fonctions
+
+if (isset($_GET['recherche'])) {
+    recherche($_GET['recherche']);
+}
+
+if (isset($_GET['recup'])) {
+    bdd();
+}
