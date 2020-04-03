2c6ba62e693dcece56237d83f066088b9e3ceb99
Mathilde <mathilde.roussier@laplateforme.io>
2020-04-03 19:03:45 +0200

    Debug systeme d'autocompletion

--- js/autocompletion.js
+++ js/autocompletion.js
@@ -1,30 +1,24 @@
-$(document).ready(function(){
-    $('#search').keyup(function(){
+$(document).ready(function () {
+    $('#search').keyup(function () {
         $.ajax({
             method: "GET",
             url: "auto.php",
             data: 'search=' + $('#search').val(),
             datatype: "json",
-            success: function(datatype){
+            success: function (datatype) {
                 var data = JSON.parse(datatype);
-                //console.log(data);
+                // console.log(data);
                 var valeur = $('#search').val();
                 $('#result').empty();
-                var i = 0;
-                for(j = 0 ; j < data.length ; ++j){
-                    for(i = 0 ; i < Object.values(data[j]).length ; ++i){
-                        if(Object.values(data[j])[i].includes(valeur) == true){
-                            console.log(Object.values(data[j])[i]);
-                            //$('#result').append('<div class="liste" id="">'+Object.values(data[j])[i]+'</div>');
+                for (j = 0; j < data.length; ++j) {
+                    for (i = 0; i < Object.values(data[j]).length; ++i) {
+                        if ((Object.values(data[j])[i].toLowerCase()).includes(valeur.toLowerCase()) == true) {
+                            $('#result').append('<div class="liste" id="">' + Object.values(data[j])[i] + '</div>');
                         }
                     }
-                }
-                
-                if($('#search').val() == ""){
-                    $('.liste').remove();
                 }
             }
         })
     })
-    
+
 })
